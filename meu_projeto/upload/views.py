5146d3736c27b06e8a1c83308a9bf8d701116fec
cardoso <cardoso.gab@outlook.com>
2025-05-18 17:52:26 -0300

    rederizando file content

--- meu_projeto/upload/views.py
+++ meu_projeto/upload/views.py
@@ -10,19 +10,32 @@ def upload(request):
     return render(request, "upload/upload.html")
 
 def upload_this(request):
+    file_content = None  
+    uploaded_file = None
+    filename = None
+
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             uploaded_file = request.FILES['file']
             file_content = uploaded_file.read().decode('utf-8')
+            filename = uploaded_file.name
 
-    if uploaded_file.name.endswith('.ris'):
-        print('Ris file recieved')
+        if uploaded_file.name.endswith('.ris'):
+            print('Ris file recieved')
 
-    elif uploaded_file.name.endswith('.bib') or uploaded_file.endswith('.bibtex'):
-        print('BibTex file received')
-        return render(request, {'filename': uploaded_file.name})
+        elif uploaded_file.name.endswith('.bib') or uploaded_file.endswith('.bibtex'):
+            print('BibTex file received')
+            return render(request, {'filename': uploaded_file.name})
+        else:
+             print('Unsupported file type')
+        
+
     else:
         form = UploadFileForm()
 
-    return render(request, 'upload/upload_this.html', {'form': form})
+    return render(request, 'upload/upload_this.html', {
+        'form': form,
+        'file_content': file_content,
+        'filename': filename
+    })
