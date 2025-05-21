79ee86561b27f4097c1833f058037465c5835f40
cardoso <cardoso.gab@outlook.com>
2025-05-21 10:31:52 -0300

    updating requiremets.txt

--- meu_projeto/meu_projeto/urls.py
+++ meu_projeto/meu_projeto/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('analitcs/', include('analiticts.urls')),
     path('upload/', include('upload.urls')),
     path('admin/', admin.site.urls),
 ]
