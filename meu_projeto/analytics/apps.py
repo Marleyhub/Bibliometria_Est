0e3a334da5717d2fe5785d1e9719c8a3bc6ac108
cardoso <cardoso.gab@outlook.com>
2025-05-22 12:48:53 -0300

    fixing settings.py

--- meu_projeto/analytics/apps.py
+++ meu_projeto/analytics/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
-class AnalitcsConfig(AppConfig):
+class AnalyticsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'analitcs'
+    name = 'analytics'
