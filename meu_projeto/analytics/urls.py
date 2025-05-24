b940a08c300264ea3494c7190d4ea1a814b35a38
cardoso <cardoso.gab@outlook.com>
2025-05-23 22:05:12 -0300

    creating route for cientific_prod on urls.py

--- meu_projeto/analytics/urls.py
+++ meu_projeto/analytics/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("author_analytics", views.author_analytics,  name="author_analytics"),
+    path("cientific_prod", views.cientific_prod,  name="cientific_prod")
 ]
