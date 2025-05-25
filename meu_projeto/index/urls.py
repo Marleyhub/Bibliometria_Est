b360c5d9c1c665554aa1b66ebd3352851c6fac25
cardoso <cardoso.gab@outlook.com>
2025-05-25 11:51:02 -0300

    routing for get to the upload template

--- meu_projeto/index/urls.py
+++ meu_projeto/index/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('begin_upload', views.begin_upload, name='begin_upload')
 ]
--- meu_projeto/index/views.py
+++ meu_projeto/index/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index/index.html')
+
+def begin_upload(request):
+    return render(request,'upload/upload.html')
